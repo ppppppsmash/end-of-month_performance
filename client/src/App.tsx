928a3b06183ed99be510772b9d1a62661e3e521c
ppppppsmash <pppppp.work@gmail.com>
2023-05-10 22:46:21 +0900

    mod:レイアウト

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,58 +9,79 @@ const App: FC = (): JSX.Element => {
 
   const [tasksPercentages, setTasksPercentages] = useState([
     { label: '車買取', time: '' },
-    { label: '車販売', time: '' },
-    { label: '廃車ツヨシ', time: '' },
-    { label: '自動車保険', time: '' },
-    { label: '自動車保険法人', time: '' },
-    { label: '生命保険', time: '' },
-    { label: 'ペット保険', time: '' },
-    { label: '火災保険', time: '' },
-    { label: '保険相談ナビ', time: '' },
-    { label: 'カードローン', time: '' },
     { label: 'マネーフィックス', time: '' },
-    { label: '火災保険（ネット型）', time: '' },
-    { label: '自動車保険（楽天IP）', time: '' },
-    { label: 'メルマガ・他', time: '' },
-    { label: '引越し', time: '' },
-    { label: 'Living', time: '' },
-    { label: '引越しOSS', time: '' },
-    { label: '引越し大手', time: '' },
-    { label: '生活ストック', time: '' },
-    { label: '電気LP', time: '' },
     { label: 'エネルギー', time: '' },
+    { label: '車販売', time: '' },
+    { label: '火災保険（ネット型）', time: '' },
     { label: '通信制高校', time: '' },
+    { label: '廃車ツヨシ', time: '' },
+    { label: '自動車保険（楽天IP）', time: '' },
     { label: 'ピアノ買取', time: '' },
+    { label: '自動車保険', time: '' },
+    { label: 'メルマガ・他', time: '' },
     { label: '不動産売却', time: '' },
+    { label: '自動車保険法人', time: '' },
+    { label: '引越し', time: '' },
     { label: '結婚', time: '' },
+    { label: '生命保険', time: '' },
+    { label: 'Living', time: '' },
     { label: 'セキュリティ', time: '' },
+    { label: 'ペット保険', time: '' },
+    { label: '引越しOSS', time: '' },
     { label: '老人ホーム', time: '' },
+    { label: '火災保険', time: '' },
+    { label: '引越し大手', time: '' },
     { label: '宅配ごはん', time: '' },
+    { label: '保険相談ナビ', time: '' },
+    { label: '生活ストック', time: '' },
     { label: 'たのめーる', time: '' },
+    { label: 'カードローン', time: '' },
+    { label: '電気LP', time: '' },
     { label: '補聴器', time: '' },
     { label: '全社共通', time: '' },
   ]);
+
+  const [totalTime, setTotalTime] = useState<number>(0);
   const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-  const data = tasksPercentages.map((task, index) => ({
+  const datas = tasksPercentages.map((task, index) => ({
     name: task.label,
-    value: Number(task.time),
+    value: parseFloat(task.time),
     color: COLORS[index % COLORS.length],
   }));
+
+  const dataTest = ()  => {
+    datas.map((data) => {
+      if(!data.value) {
+        data.name = ''
+      }
+      return data
+    })
+  }
 
 
   console.log(tasksPercentages)
 
+  const testData: {label: string, time: string}[] = [];
+
   const handlePercentageChange = (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
     const newTime = event.target.value;
-    setTasksPercentages((prevTasks) =>
-      prevTasks.map((task, i) => {
-        if (i === index) {
-          return { ...task, time: newTime };
-        }
-        return task;
-      })
-    );
+
+    const newTask = { label: tasksPercentages[index].label, time: newTime }
+    const newTasks = [...tasksPercentages];
+    newTasks[index].time += newTask.time;
+    //newTasks[index].time = newTime;
+    // setTasksPercentages((prevTasks) =>
+    //   prevTasks.map((task, i) => {
+    //     if (i === index) {
+    //       return { ...task, time: newTime };
+    //     }
+    //     return task;
+    //   })
+    // );
+    setTasksPercentages(newTasks);
+    console.log(newTasks)
+    setTotalTime(newTasks.reduce((sum, task) => sum + Number(task.time), 0));
   };
 
   const [tasksTimes, setTasksTimes] = useState([
@@ -77,33 +98,38 @@ const App: FC = (): JSX.Element => {
     { label: 'メルマガ 保険', time: '' },
     { label: '損害保険見直し本舗', time: '' },
     { label: '保険その他作業', time: '' },
-  ])
+  ]);
+
+  const formatTime = (time: number): string => {
+    const hours = Math.floor(time);
+    const minutes = Math.round((time - hours) * 60);
+    return `${hours}時間${minutes}分`;
+  };
 
   return (
     <div>
       <Title title={title} />
       <div className="flex flex-wrap w-full space-x-4 items:center mt-8 px-6">
-        <div className="flex-1 w-5/12 border border-solid rounded border-black p-8 overflow-x-none h-[90vh] overflow-y-scroll">
-          <div className="mb-12">
+        <div className="flex-1 w-5/12 border border-solid rounded border-black px-6 py-8 overflow-x-none h-[90vh] overflow-y-scroll">
+          <div className="mb-12 border border-solid border-black rounded p-6">
             <div className="mb-4"><h2 className="text-2xl">業務割合</h2></div>
             <div>
-              <p>合計時間：</p>
-              <p>合計割合：</p>
+              <p>合計時間：{formatTime(totalTime)}</p>
             </div>
-              <div className='flex flex-wrap m-10 gap-x-10'>
-              {tasksPercentages.map((task, index) => (
-                <Input key={index} label={task.label} time={task.time} onChange={(e: any) => handlePercentageChange(e, index)} />
-              ))}
-              </div>
+            <div className='flex flex-wrap'>
+            {tasksPercentages.map((task, index) => (
+              <Input key={index} label={task.label} time={task.time} onChange={(e: any) => handlePercentageChange(e, index)} />
+            ))}
+            </div>
           </div>
-          <div>
+          <div className="mb-12 border border-solid border-black rounded p-6">
             <div className="mb-4"><h2 className="text-2xl">工数</h2></div>
             <div>
             <p>合計時間：</p>
             </div>
-            <div className='flex flex-wrap m-10 gap-x-10'>
+            <div className='flex flex-wrap'>
               {tasksTimes.map((task, index) => (
-                <Input key={index} label={task.label} time={task.time} onChange={(e: any) => handlePercentageChange(e, index)} />
+                <Input key={index} label={task.label} time={task.time} onChange={(e: any) => {}} />
               ))}
             </div>
           </div>
@@ -111,20 +137,29 @@ const App: FC = (): JSX.Element => {
         <div className="flex-1 w-5/12 border border-solid rounded border-black p-8">
           <div className="w-full mx-auto mt-20">
             <PieChart width={700} height={400}>
+              { datas.map((data, index) => {
+                if(isNaN(data.value)) {
+                  console.log(data)
+                  //datas.splice(index, 1);
+                }
+              }) &&
               <Pie
-                data={data}
+                data={datas}
                 dataKey="value"
                 nameKey="name"
                 cx="50%"
                 cy="50%"
                 outerRadius={80}
                 fill="#8884d8"
-                label={({ name, value }) => `${name}：${(((value) * 100).toFixed(1))}%`}
+                label={({ name, value }) =>
+                  `${name}：${(((value / totalTime) * 100).toFixed(1))}%`
+                }
               >
-                {data.map((entry, index) => (
+                {datas.map((entry, index) => (
                   <Cell key={`cell-${index}`} fill={entry.color} />
                 ))}
               </Pie>
+              }
             </PieChart>
           </div>
         </div>
--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -8,14 +8,23 @@ interface Props {
 
 const Input: FC<Props> = ({label, time, onChange}): JSX.Element => {
     return (
-        <div className="relative gap-x-3 w-5/12">
+        <div className="relative gap-x-3 w-4/12">
             <>
-            <div>
-                <div className="text-sm leading-6">
+            <div className="flex mt-3 items-center">
+                <div className="text-sm leading-6 w-36">
                     <label className="font-medium text-gray-900">{label}</label>
                 </div>
                 <div>
-                    <input onChange={(e) => onChange(e)} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" type="text" value={time} />
+                    <input
+                        type="text"
+                        className="block w-4/12 rounded-md border-0 py-1.5
+                        text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+                         placeholder:text-gray-400 focus:ring-2
+                         focus:ring-inset focus:ring-indigo-600
+                         sm:text-sm sm:leading-6"
+                         value={time}
+                         onChange={(e) => onChange(e)}
+                    />
                 </div>
             </div>
             </>
