ea3c36499611f16ff08064139e4ccc7938660d92
ppppppsmash <pppppp.work@gmail.com>
2023-05-17 22:35:07 +0900

    mod: input仕様

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -18,11 +18,9 @@ const Input: FC<Props> = ({label, time, onChange}): JSX.Element => {
                 <div>
                     <input
                         type="text"
-                        className="block w-4/12 rounded-md border-0 py-1.5
-                        text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
-                         placeholder:text-gray-400 focus:ring-2
-                         focus:ring-inset focus:ring-indigo-600
-                         sm:text-sm sm:leading-6"
+                        className='outline-none p-0 w-[50px] h-[30px]
+                        border-b-2 border-black-300 text-sm
+                        focus:border-b-2 focus:border-black'
                          value={time}
                          onChange={(e) => onChange(e)}
                     />
